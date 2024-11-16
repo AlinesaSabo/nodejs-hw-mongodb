3d2fd091f269f31c3cf32e66a7dd9defff577c31
Alinesa <alinesa322@gmail.com>
2024-11-16 13:45:39 +0200

    push

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,11 +2,6 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 export const initMongoConnection = async () => {
-  console.log('MONGODB_USER:', process.env.MONGODB_USER);
-  console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
-  console.log('MONGODB_URL:', process.env.MONGODB_URL);
-  console.log('MONGODB_DB:', process.env.MONGODB_DB);
-
   const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
 
   try {
